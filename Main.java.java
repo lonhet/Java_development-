a49e7be9275452a1b643fc7a9c2e517fbf181f3d
lonhet <lonhetmp@gmail.com>
2026-01-14 21:07:50 +0300

    Add files via upload

--- Main.java.java
+++ Main.java.java
@@ -2,21 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Point p = new Point(3, 4);
-        System.out.println("Point: " + p);
-        System.out.println("Distance 2D: " + p.distanceFromOrigin());
+        Point p = new Point(1, 0);
+        p.rotate(Math.PI / 2); 
+        System.out.println("2D rotate: " + p);
 
-        Point3D p3 = new Point3D(1, 2, 2);
-        System.out.println("\nPoint3D: " + p3);
-        System.out.println("Distance 3D: " + p3.distanceFromOrigin());
+        Point3D p3 = new Point3D(1, 1, 1);
 
-        p3.setLocation(5, 6);
-        System.out.println("\nAfter setLocation(5,6): " + p3);
+        p3.rotateX(Math.PI / 2);
+        System.out.println("After X: " + p3);
 
-        Point3D a = new Point3D(0, 0, 0);
-        Point3D b = new Point3D(10, 10, 10);
+        p3.rotateY(Math.PI / 2);
+        System.out.println("After Y: " + p3);
 
-        System.out.println("\nIs in box: " + p3.isInBox(a, b));
-        System.out.println("Is in sphere (r=5): " +
-                p3.isInSphere(new Point3D(0,0,0), 5));}
+        p3.rotateZ(Math.PI / 2);
+        System.out.println("After Z: " + p3);}
 }
